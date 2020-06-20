bdb3785465ae49a8d9092f094e4b10b895dcf162
Rafael Mello <rafaelmello0715@outlook.com>
2020-06-20 07:32:17 -0300

    feat: config staticfiles

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
+import { static as staticfiles } from 'express';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { resolve } from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.use('/uploads', staticfiles(resolve(__dirname, '..', 'tmp', 'uploads')));
+  await app.listen(3333);
 }
 bootstrap();
